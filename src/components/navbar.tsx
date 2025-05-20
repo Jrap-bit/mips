913cec94525e861d824c072d7883c9fbccb76427
jrap-bit <namanpandey10@gmail.com>
2025-05-20 23:29:39 +0530

    Initial Commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,21 @@
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="bg-gray-800">
+                <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                    <div className="relative flex items-center justify-between h-16">
+                        <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                            {/* Mobile menu button*/}
+                        </div>
+                        <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                            <div className="flex-shrink-0">
+                                <h1 className="text-white text-lg font-bold">My Website</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+
+}
